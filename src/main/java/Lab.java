f08dfb4ff528cf12262b7522246536dea2dd4500
sushmishra87 <sm.sushmimishra@gmail.com>
2023-02-20 21:19:27 +0000

    Completed Lab Instantiation Test

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,8 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object test = new Object();
+        
+        return test;
     }
 }
